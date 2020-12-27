7c0b95eceb4851e1e59d73aef6501efe1b127429
Ahmet Barış Durak <58290013+Akamerino@users.noreply.github.com>
2020-12-27 21:34:21 +0300

    Update odev3.py

--- odev3.py
+++ odev3.py
@@ -13,7 +13,7 @@ temp = 0
 
 while turn > 0:
 
-    char = input("Enter a char! ")
+    char = input("\nEnter a char! ")
 
     if char in guess:
         print("You already said this character")
@@ -25,12 +25,20 @@ while turn > 0:
             for x in range(0,len(word)):
                 if char == word[x]:
                     temp += 1
+                    print("You found one of the characters!")
+
+                    for i in range(0,len(word)):
+                        if word[i] in guess:
+                            print(word[i], end="")
+                        else:
+                            print("_", end="")
+
         else:
             print("Wrong character!")
             turn -= 1
 
     if temp == length:
-        print("You found the word")
+        print("\nYou found the word")
         break
 
 if turn == 0:
